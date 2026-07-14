fdc52715b60acca1439c8329c70334ac70664a4b
Elijah Morgan <161932463+Scooterking38@users.noreply.github.com>
2026-07-14 14:57:35 +0100

    Update my_app.ex

--- lib/my_app.ex
+++ lib/my_app.ex
@@ -3,21 +3,28 @@ defmodule MyApp do
 
   def start(_type, _args) do
     children = [
-      {Postgrex,
-       [
-         name: MyApp.DB,
-         url: System.get_env("DATABASE_URL"),
-         pool_size: 5
-       ]},
-      {Plug.Cowboy,
-       scheme: :http,
-       plug: MyApp.Router,
-       options: [port: 4000]}
+      {
+        Postgrex,
+        [
+          name: MyApp.DB,
+          url: System.get_env("DATABASE_URL"),
+          pool_size: 5
+        ]
+      },
+      {
+        Plug.Cowboy,
+        [
+          scheme: :http,
+          plug: MyApp.Router,
+          options: [port: 4000]
+        ]
+      }
     ]
 
     Supervisor.start_link(
       children,
-      strategy: :one_for_one
+      strategy: :one_for_one,
+      name: MyApp.Supervisor
     )
   end
 end
