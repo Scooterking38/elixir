ffe5fc6228924fdcd639266b2a05d8c2f0ec3e19
Elijah Morgan <161932463+Scooterking38@users.noreply.github.com>
2026-07-14 14:36:48 +0100

    Update my_app.ex

--- lib/my_app.ex
+++ lib/my_app.ex
@@ -1,8 +1,27 @@
 defmodule MyApp do
   def main do
+    url = System.get_env("DATABASE_URL")
+
+    IO.puts("Connecting...")
+
+    uri = URI.parse(url)
+
+    IO.inspect(
+      %{
+        scheme: uri.scheme,
+        host: uri.host,
+        path: uri.path
+      },
+      label: "Parsed URL"
+    )
+
     {:ok, pid} =
       Postgrex.start_link(
-        url: System.get_env("DATABASE_URL")
+        hostname: uri.host,
+        username: uri.userinfo |> String.split(":") |> hd(),
+        password: uri.userinfo |> String.split(":") |> List.last(),
+        database: String.trim_leading(uri.path, "/"),
+        ssl: true
       )
 
     result = Postgrex.query!(pid, "SELECT NOW()", [])
