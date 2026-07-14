4ff8aef119857b48f2747b69b686f0a756c23af3
Elijah Morgan <161932463+Scooterking38@users.noreply.github.com>
2026-07-14 09:47:58 +0100

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
-defmodule MyProject.MixProject do
+defmodule MyApp.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :my_project,
+      app: :my_app,
       version: "0.1.0",
       elixir: "~> 1.14",
-      start_permanent: Mix.env() == :prod,
-      deps: []
+      deps: [{:plug_cowboy, "~> 2.6"}]
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {MyApp.Application, []}
     ]
   end
 end
