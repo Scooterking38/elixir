602dbfe67c29ef17652bf8b71c4e6f98397bc060
Elijah Morgan <161932463+Scooterking38@users.noreply.github.com>
2026-07-14 14:22:17 +0100

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,23 +5,19 @@ defmodule MyApp.MixProject do
     [
       app: :my_app,
       version: "0.1.0",
-      elixir: "~> 1.14",
-      deps: deps()
+      elixir: "~> 1.17"
     ]
   end
 
   def application do
     [
-      extra_applications: [:logger],
-      mod: {MyApp.Application, []}
+      extra_applications: [:logger]
     ]
   end
 
   defp deps do
     [
-      {:plug_cowboy, "~> 2.6"},
-      {:ecto_sql, "~> 3.13"},
-      {:postgrex, ">= 0.0.0"}
+      {:postgrex, "~> 0.19"}
     ]
   end
 end
