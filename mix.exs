242b8a5ec735b7b3eedd3663e192b35b78b08f68
Elijah Morgan <161932463+Scooterking38@users.noreply.github.com>
2026-07-14 09:37:50 +0100

    Create mix.exs

---
+++ mix.exs
@@ -0,0 +1,13 @@
+defmodule MyProject.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :my_project,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      start_permanent: Mix.env() == :prod,
+      deps: []
+    ]
+  end
+end
