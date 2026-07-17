994f20cd80b1a774a5f7081868bfa5685df4e9fa
Elijah Morgan <161932463+Scooterking38@users.noreply.github.com>
2026-07-17 12:21:01 +0100

    Add initial router implementation with Plug

---
+++ lib/router.ex
@@ -0,0 +1,19 @@
+defmodule MyApp.Router do
+  use Plug.Router
+
+  plug :match
+  
+  # Parsers must handle both urlencoded forms and json if necessary
+  plug Plug.Parsers,
+    parsers: [:urlencoded],
+    pass: ["*/*"],
+    json_decoder: Jason
+
+  plug :fetch_cookies
+  plug :dispatch
+
+  # 1. The Home Page (Using proper POST actions)
+  get "/:url" do
+    send_resp(conn, 200, "you visited ${url}")
+  end
+end
